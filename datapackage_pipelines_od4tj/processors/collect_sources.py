970120b252d4e3d961575cb1eb57945323178f7d
Georgiana Bere <bere.georgi@gmail.com>
2017-07-22 22:39:11 +0300

    Add concatenation pipeline

--- datapackage_pipelines_od4tj/processors/collect_sources.py
+++ datapackage_pipelines_od4tj/processors/collect_sources.py
@@ -1,19 +1,30 @@
-import os
-
-from datapackage_pipelines.wrapper import spew, ingest
-
+import boto3
+import logging
+from datapackage_pipelines.wrapper import ingest, spew
 
 params, dp, res_iter = ingest()
+dp.setdefault('resources', [])
 
-base = params['base_path']
+s3client = boto3.client('s3')
+truncated = True
+continuation_token = None
+aws_params = {
+    'Bucket': params['bucket'],
+    'Prefix': params['path'],
+}
 
-count = 0
-for dirpath, dirnames, filenames in os.walk(base):
-    for filename in filenames:
-        if filename.endswith('.csv'):
+while truncated is True:
+    if continuation_token is not None:
+        aws_params['ContinuationToken'] = continuation_token
+    response = s3client.list_objects_v2(**aws_params)
+    truncated = response['IsTruncated']
+    continuation_token = response.get('NextContinuationToken')
+    for resource in response['Contents']:
+        resource_path = resource['Key']
+        if resource_path.endswith('.csv'):
             dp['resources'].append({
-                'name': 'res{}'.format(len(dp['resources'])),
-                'url': os.path.join(dirpath, filename)
+                'name': resource_path.replace('/', '-'),
+                'url': 'https://{0}.s3.amazonaws.com/{1}'.format(params['bucket'], resource_path),
             })
 
 spew(dp, res_iter)
