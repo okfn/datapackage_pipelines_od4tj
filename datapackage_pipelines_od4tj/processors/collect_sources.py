cde11aa8212276170da3d366e0910708ef8513a4
Adam Kariv <adam.kariv@gmail.com>
2017-07-24 09:25:48 +0100

    Simplify concatenation pipeline

--- datapackage_pipelines_od4tj/processors/collect_sources.py
+++ datapackage_pipelines_od4tj/processors/collect_sources.py
@@ -1,30 +1,29 @@
 import boto3
 import logging
+
+from boto import connect_s3
+from botocore import client
+from boto.s3.connection import OrdinaryCallingFormat
 from datapackage_pipelines.wrapper import ingest, spew
 
 params, dp, res_iter = ingest()
 dp.setdefault('resources', [])
 
-s3client = boto3.client('s3')
-truncated = True
-continuation_token = None
-aws_params = {
-    'Bucket': params['bucket'],
-    'Prefix': params['path'],
-}
+# s3client = boto3.client('s3', config={'calling_format': OrdinaryCallingFormat()})
+conn = connect_s3(
+    is_secure=True,               # require ssl
+    calling_format = OrdinaryCallingFormat(),
+    host = 's3-eu-west-1.amazonaws.com'
+)
+bucket = conn.get_bucket(params['bucket'])
 
-while truncated is True:
-    if continuation_token is not None:
-        aws_params['ContinuationToken'] = continuation_token
-    response = s3client.list_objects_v2(**aws_params)
-    truncated = response['IsTruncated']
-    continuation_token = response.get('NextContinuationToken')
-    for resource in response['Contents']:
-        resource_path = resource['Key']
-        if resource_path.endswith('.csv'):
-            dp['resources'].append({
-                'name': resource_path.replace('/', '-'),
-                'url': 'https://{0}.s3.amazonaws.com/{1}'.format(params['bucket'], resource_path),
-            })
+response = bucket.list(prefix=params['path'])
+for resource in response:
+    resource_path = resource.name
+    if resource_path.endswith('.csv'):
+        dp['resources'].append({
+            'name': resource_path.replace('/', '-'),
+            'url': 'http://{0}.s3.amazonaws.com/{1}'.format(params['bucket'], resource_path),
+        })
 
 spew(dp, res_iter)
